51d5129ea2113353ab7ed58efddf8ffe8d1dd745
H12345J <118468337+H12345J@users.noreply.github.com>
2022-11-17 22:24:45 +0530

    Add files via upload

---
+++ Python_Assignment_Day1.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to datatypes in python:
+
+
+# In[ ]:
+
+
+1.String====> str
+2.Numbers---> integers===>int floats ====> float(decimals)
+3.lists=====> list
+4.tuples====> tuple
+5.dictionaries====> dict
+
+
+# In[ ]:
+
+
+classification of datatypes:
+    broadly classified into two catagories
+    1. Mutable datatypes====>which we edit and alter====>flexible in nature
+    2. Immutable datatypes====>which we can not edit and alter====>fixed in nature
+
+
+# In[ ]:
+
+
+Introduction to string datatype:
+    definition: A sting is nothing but a series of characters declared in quotes
+        classification: It is classified as an immutable datatype (fixed datatype)
+            
+How to declare / define the string datatype:
+    1. single quote ===>''
+    2. double quotes====>""
+    3. triple quotes====>""""""
+
+
+# In[1]:
+
+
+name1 = 'jyoti' #single quote
+print(name1)
+
+
+# In[2]:
+
+
+name2 = "Harry" #double quotes
+print(name2)
+
+
+# In[3]:
+
+
+name3 = """Shinchan""" #triple quote
+print(name3)
+
+
+# In[ ]:
+
+
+#is used for commenting the lines---> it will not impact the code 
+
+
+# In[ ]:
+
+
+Introduction to string methods:
+
+
+# In[5]:
+
+
+type(name1)
+
+
+# In[6]:
+
+
+type(name2)
+
+
+# In[7]:
+
+
+type(name3)
+
+
+# In[ ]:
+
+
+Thank you for Day1
+
